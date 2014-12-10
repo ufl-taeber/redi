59f9296a3f6311a26c87d7e22ce1342627a420cb
Taeber Rapczak <taeber@ufl.edu>
2014-12-10 16:32:21 -0500

    Add XML Reader and more customization options

--- redi/input.py
+++ redi/input.py
@@ -1,58 +1,159 @@
 import csv
+import decimal
+import time
+
+from lxml import etree
 
 HEADER_LOINC_CODE = 'loinc_code'
 HEADER_LOINC_COMPONENT = 'loinc_component'
 HEADER_RESULT = 'result'
 HEADER_UNITS = 'units'
+HEADER_SUBJECT = 'study_id'
+HEADER_TIMESTAMP = 'date_time_stamp'
+ELEMENT_SUBJECT = 'subject'
 
 
-class EpicExportReader(object):
-    def __init__(self, headers=None):
+class CsvReader(object):
+    def __init__(self, clinical_components_service, headers=None, timestamp_format=None):
         names = (HEADER_LOINC_CODE, HEADER_LOINC_COMPONENT, HEADER_RESULT,
-                 HEADER_UNITS)
+                 HEADER_UNITS, HEADER_SUBJECT, HEADER_TIMESTAMP)
         self._headers = dict(zip(names, names))
         if headers:
             self._headers.update(headers)
 
+        self._clinical_components_service = clinical_components_service
+        self._timestamp_format = timestamp_format
+
     def read(self, stream):
         reader = csv.DictReader(stream)
-        return ClinicalRecords(list(reader), self._headers)
+        return ClinicalRecords(list(reader), self._headers,
+                               self._clinical_components_service,
+                               self._timestamp_format)
+
+
+class FlatXmlReader(CsvReader):
+    class _LxmlWrapper(object):
+        """Wraps an LXML element to support an indexer"""
+        def __init__(self, element):
+            self._element = element
+
+        def __getitem__(self, item):
+            return self._element.findtext(item)
+
+    def read(self, stream):
+        tree = etree.parse(stream)
+        return ClinicalRecords([self._LxmlWrapper(s)
+                                for s in tree.xpath('//' + ELEMENT_SUBJECT)],
+                               self._headers, self._clinical_components_service,
+                               self._timestamp_format)
 
 
 class ClinicalRecords(object):
 
-    def __init__(self, iterable, headers):
+    def __init__(self, iterable, headers, clinical_components_service,
+                 timestamp_format):
         self._records = iterable
         self._headers = headers
+        self._clinical_components_service = clinical_components_service
+        self._timestamp_format = timestamp_format
 
     def __len__(self):
         return len(self._records)
 
     def __getitem__(self, index):
-        return ClinicalRecord(self._records[index], self._headers)
+        return ClinicalRecord(self._records[index], self._headers,
+                              self._clinical_components_service,
+                              self._timestamp_format)
+
+
+class Result(object):
+    def __init__(self, component, value):
+        self._component = component
+        self._verbatim = value
+
+    @property
+    def value(self):
+        return self._component.type(self._verbatim)
+
+    @property
+    def verbatim(self):
+        return self._verbatim
 
 
 class ClinicalRecord(object):
-    def __init__(self, record, headers):
+    def __init__(self, record, headers, clinical_components_service,
+                 timestamp_format):
         self._record = record
         self._headers = headers
+        loinc_code = self._record[self._headers[HEADER_LOINC_CODE]]
+        self._component = clinical_components_service[loinc_code]
+        self._timestamp_format = timestamp_format
 
     @property
     def component(self):
-        loinc_component = self._record[self._headers[HEADER_LOINC_COMPONENT]]
-        loinc_code = self._record[self._headers[HEADER_LOINC_CODE]]
-        return ClinicalComponent(loinc_component, loinc_code)
+        return self._component
 
     @property
     def result(self):
-        return float(self._record[self._headers[HEADER_RESULT]])
+        return Result(self.component, self._record[self._headers[HEADER_RESULT]])
+
+    @property
+    def subject(self):
+        return self._record[self._headers[HEADER_SUBJECT]]
+
+    @property
+    def timestamp(self):
+        return time.strptime(self._record[HEADER_TIMESTAMP], self._timestamp_format)
 
     @property
     def units(self):
+        if not self.component.has_units:
+            raise NotImplemented()
         return self._record[self._headers[HEADER_UNITS]]
 
 
+def boolean(text):
+    return text.strip("\"'").lower() in ('y', 'yes', 't', 'true')
+
+
+def number(text):
+    return decimal.Decimal(text)
+
+
 class ClinicalComponent(object):
-    def __init__(self, name, loinc_code):
+    TYPE_NUMBER = 'number'
+    TYPE_BOOLEAN = 'boolean'
+    TYPE_TEXT = 'text'
+
+    KNOWN_TYPES = {
+        TYPE_NUMBER: number,
+        TYPE_BOOLEAN: boolean,
+        TYPE_TEXT: str,
+    }
+
+    def __init__(self, name, loinc_code, type=TYPE_NUMBER, has_units=True):
         self.name = name
         self.loinc_code = loinc_code
+        self.type = type
+        if type in self.KNOWN_TYPES:
+            self.type = self.KNOWN_TYPES[type]
+        self.has_units = has_units
+
+
+class ClinicalComponents(object):
+    def __init__(self, components):
+        self._components = dict(components)
+
+    def __getitem__(self, loinc_code):
+        return self._components[loinc_code]
+
+    @staticmethod
+    def from_csv(iterable):
+        reader = csv.DictReader(iterable, skipinitialspace=True)
+        components = {
+            (r['loinc_code'],
+             ClinicalComponent(r['loinc_component'], r['loinc_code'],
+                               r['type'] or None,
+                               r['has_units'] if r['has_units'] else True))
+            for r in reader}
+        return ClinicalComponents(components)
--- test/test_input.py
+++ test/test_input.py
@@ -1,8 +1,25 @@
+import io
+import time
 import unittest
+
 import redi.input
 
 
-class EpicExportReader(unittest.TestCase):
+class ClinicalComponentsTests(unittest.TestCase):
+    def test_from_csv(self):
+        config = """
+loinc_code, loinc_component, type, has_units,
+26464-8, Leukocytes,,,
+"26499-4","Neutrophils",,,
+"""
+        cc = redi.input.ClinicalComponents.from_csv(config.strip().split('\n'))
+
+        self.assertIsNotNone(cc['26464-8'])
+        self.assertIsNotNone(cc['26499-4'])
+        self.assertTrue(cc['26499-4'].has_units)
+
+
+class CsvReaderTests(unittest.TestCase):
 
     def test_read(self):
         csv = """
@@ -10,13 +27,17 @@ class EpicExportReader(unittest.TestCase):
 3.813,"Leukocytes","26464-8",3.8,10.8,"10*3/uL",2112-10-27,1
 3.433,"Neutrophils","26499-4",1.5,7.8,"10*3/uL",2112-10-27,1
 """
+        cc = redi.input.ClinicalComponents({
+            '26464-8': redi.input.ClinicalComponent('Leukocytes', '26464-8'),
+            '26499-4': redi.input.ClinicalComponent('Neutrophils', '26599-4'),
+        })
 
-        reader = redi.input.EpicExportReader()
+        reader = redi.input.CsvReader(cc)
         data = reader.read(csv.strip().split('\n'))
 
         self.assertEqual(2, len(data))
         self.assertEqual("26464-8", data[0].component.loinc_code)
-        self.assertEqual(3.433, data[1].result)
+        self.assertEqual(3.433, float(data[1].result.value))
         self.assertEqual("10*3/uL", data[1].units)
 
     def test_read_custom_headers(self):
@@ -25,8 +46,12 @@ class EpicExportReader(unittest.TestCase):
 3.813,"Leukocytes","26464-8",3.8,10.8,"10*3/uL",2112-10-27,1
 3.433,"Neutrophils","26499-4",1.5,7.8,"10*3/uL",2112-10-27,1
 """
+        cc = redi.input.ClinicalComponents({
+            '26464-8': redi.input.ClinicalComponent('Leukocytes', '26464-8'),
+            '26499-4': redi.input.ClinicalComponent('Neutrophils', '26599-4'),
+        })
 
-        reader = redi.input.EpicExportReader({
+        reader = redi.input.CsvReader(cc, {
             'loinc_code': 'code_of_loinc',
             'loinc_component': 'loinc_desc',
             'result': 'test_results',
@@ -36,8 +61,85 @@ class EpicExportReader(unittest.TestCase):
 
         self.assertEqual(2, len(data))
         self.assertEqual("26464-8", data[0].component.loinc_code)
-        self.assertEqual(3.433, data[1].result)
+        self.assertEqual(3.433, float(data[1].result.value))
         self.assertEqual("10*3/uL", data[1].units)
+
+    def test_read_expected_non_numeric_units(self):
+        csv = """
+"result","loinc_component","loinc_code","low","high","units","date_time_stamp","study_id"
+Yes,"Are you happy?","12345-0",,,,2112-10-27,1
+"Doe, John","Name","12345-1",,,,2112-10-27,1
+"""
+
+        cc = redi.input.ClinicalComponents({
+            '12345-0': redi.input.ClinicalComponent('Are you happy?', '12345-0',
+                                                    type='boolean',
+                                                    has_units=False),
+            '12345-1': redi.input.ClinicalComponent('Name', '12345-1', type=str,
+                                                    has_units=False)
+        })
+
+        reader = redi.input.CsvReader(cc)
+        data = reader.read(csv.strip().split('\n'))
+
+        self.assertEqual(2, len(data))
+        self.assertEqual(True, data[0].result.value)
+        self.assertEqual("Doe, John", data[1].result.value)
+
+    def test_read_date(self):
+        csv = """
+"result","loinc_component","loinc_code","low","high","units","date_time_stamp","study_id"
+0.1,"LOINCNESS Monster","1",low,high,units,2112-10-27,1
+        """
+
+        cc = redi.input.ClinicalComponents({
+            '1': redi.input.ClinicalComponent('Are you happy?', '1')
+        })
+
+        reader = redi.input.CsvReader(cc, timestamp_format='%Y-%m-%d')
+        data = reader.read(csv.strip().split('\n'))
+
+        self.assertEqual('10/27/2112', time.strftime('%m/%d/%Y', data[0].timestamp))
+
+    def test_read_xml(self):
+        xml = """<?xml version="1.0" encoding="utf8"?>
+            <study>
+                <subject>
+                    <result>3.813</result>
+                    <loinc_component>Leukocytes</loinc_component>
+                    <loinc_code>26464-8</loinc_code>
+                    <low>3.8</low>
+                    <high>10.8</high>
+                    <units>10*3/uL</units>
+                    <date_time_stamp>2112-10-27</date_time_stamp>
+                    <study_id>1</study_id>
+                </subject>
+                <subject>
+                    <result>3.433</result>
+                    <loinc_component>Neutrophils</loinc_component>
+                    <loinc_code>26499-4</loinc_code>
+                    <low>1.5</low>
+                    <high>7.8</high>
+                    <units>10*3/uL</units>
+                    <date_time_stamp>2112-10-27</date_time_stamp>
+                    <study_id>1</study_id>
+                </subject>
+            </study>"""
+
+        cc = redi.input.ClinicalComponents({
+            '26464-8': redi.input.ClinicalComponent('Leukocytes', '26464-8'),
+            '26499-4': redi.input.ClinicalComponent('Neutrophils', '26599-4'),
+        })
+
+        reader = redi.input.FlatXmlReader(cc)
+        data = reader.read(io.BytesIO(xml))
+
+        self.assertEqual(2, len(data))
+        self.assertEqual("26464-8", data[0].component.loinc_code)
+        self.assertEqual(3.433, float(data[1].result.value))
+        self.assertEqual("10*3/uL", data[1].units)
+        self.assertEqual(2, len(filter(lambda r: r.subject == '1', data)))
+        self.assertEqual(1, len(filter(lambda r: r.result.verbatim == '3.813', data)))
 
 
 if __name__ == '__main__':
